b6e28ac828b6499818550b51684cd092a41a3be7
Joe Taylor <joetaylor010@gmail.com>
2021-09-08 23:11:36 -0400

    Update TrafficLight.ino
    Updates for running two digits of the seven segment display.

--- Lab 1/TrafficLight/TrafficLight.ino
+++ Lab 1/TrafficLight/TrafficLight.ino
@@ -1,6 +1,7 @@
 // TrafficLight.ino
 // Joseph Taylor, Elizabeth Brown, Cody Bellec
 // this code incorporates components of the Elegoo sample code, found at www.elegoo.com
+// also incorporates components of https://www.circuitbasics.com/arduino-7-segment-display-tutorial/
 
 // revision history:
 // 9/8/21 jct - file init
@@ -11,9 +12,13 @@ int red = 3;     // red LED; through a 220ohm to GND
 int yellow = 4;  // yellow LED; through a 220ohm to GND
 int green = 5;   // green LED; through a 220ohm to GND
 int buzzer = 13; // active buzzer; positive to pin 13, negative to GND
-int data = 8;    // 74HC595 pin 8 DS
-int latch = 9;   // 74HC595 pin 9 STCP
-int clock = 10;  // 74HC595 pin 10 SHCP
+
+// pin declarations for 7-segment display
+int data = 6;    // 74HC595 pin 6 DS
+int latch = 7;   // 74HC595 pin 7 STCP
+int clock = 8;   // 74HC595 pin 8 SHCP
+int dig1 = 9;    // cathode for digit 1
+int dig2 = 10;   // cathode for digit 2
 
 unsigned char table[]=
 {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c
@@ -28,6 +33,8 @@ void setup() {
   pinMode(latch,OUTPUT);
   pinMode(clock,OUTPUT);
   pinMode(data,OUTPUT);
+  pinMode(dig1,OUTPUT);
+  pinMode(dig2,OUTPUT);
 }
 
 void Display(unsigned char num)
@@ -42,6 +49,9 @@ void setLight(int color) {
   digitalWrite(yellow, color == yellow);
   digitalWrite(green, color == green);
 }
+
+// the code below works, but the ELEGOO kit only provides code that displays all 8 segments at the same time.
+
 void loop() {
   if (digitalRead(button)) {
     digitalWrite(buzzer, HIGH);
@@ -49,47 +59,22 @@ void loop() {
   else {
     digitalWrite(buzzer, LOW);
   }
-  delay(1000);
   setLight(green);
-  delay(1000);
+  delay(500);
   setLight(yellow);
-  delay(1000);
+  delay(500);
   setLight(red);
-
-  // setLight, button, and buzzer all work as intended
-  // need to wire up 7-segment and make sure samples below work
-  // also need to look up how the heck interrupts work on arduino
-  
-  /*
-  Display(1);
   delay(500);
-  Display(2);
-  delay(500);
-  Display(3);
-  delay(500);
-  Display(4);
-  delay(500);
-  Display(5);
-  delay(500);
-  Display(6);
-  delay(500);
-  Display(7);
-  delay(500);
-  Display(8);
-  delay(500);
-  Display(9);
-  delay(500);
-  Display(10);
-  delay(500);
-  Display(11);
-  delay(500);
-  Display(12);
-  delay(500);
-  Display(13);
-  delay(500);
-  Display(14);
-  delay(500);
-  Display(15);
-  delay(500);
-  */
+  digitalWrite(dig1, HIGH);
+  digitalWrite(dig2, LOW);
+  for (int i=0; i<10; i++) {
+    Display(i);
+    delay(500);
+  }
+  digitalWrite(dig1, LOW);
+  digitalWrite(dig2, HIGH);
+  for (int i=0; i<10; i++) {
+    Display(i);
+    delay(500);
+  }
 }
